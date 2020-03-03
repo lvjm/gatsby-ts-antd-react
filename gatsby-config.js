7cf038769127ece56abe52e60cc579d61221a5a1
may <may@lv.com>
2020-03-03 18:27:57 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,18 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "SWAPI",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "swapi",
+        // Url to query from
+        url: "https://service-5ixybmdh-1257983906.sh.apigw.tencentcs.com",
+      },
+    },
+  ],
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Login from "../components/login"
+
+const App = () => (
+  <div>
+    <Login></Login>
+  </div>
+)
+
+export default App
